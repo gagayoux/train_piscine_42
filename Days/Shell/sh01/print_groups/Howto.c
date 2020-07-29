c9ea5e6fa7d9ff3bea52aacb2a863722fdbe42e5
user42 <williams.ozserttas05@gmail.com>
2020-07-29 13:17:49 +0200

    do op a corriger

--- Days/Shell/sh01/print_groups/Howto.c
+++ Days/Shell/sh01/print_groups/Howto.c
@@ -5,12 +5,9 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/29 12:46:52 by user42            #+#    #+#             */
-/*   Updated: 2020/07/29 13:08:38 by user42           ###   ########.fr       */
+/*   Created: 2020/07/29 12:08:59 by user42            #+#    #+#             */
+/*   Updated: 2020/07/29 12:09:01 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* 42		string	42		42	file
-le premier 42 correspond au nombre de decalage en octet dans le fichier a tester
-le string 42 correspond a la chaine 42
-le 42 file est le type de fichier a tester 
+/* 
